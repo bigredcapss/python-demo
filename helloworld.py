b0770e36c354ac22aa447668285c5110e6f5de9f
wangen <wangen@soulapp.cn>
2025-02-23 13:01:02 +0800

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+
+# filename : helloworld.py
+# author by : peanut
+# date : 2025/2/23
+
+print('hello,world!')
+
---
+++ test_operations.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+
+# filename : test_operations.py
+# author by : peanut
+# date : 2025/2/23
+# 用户输入
+num1 = input('请输入第一个数字：')
+num2 = input('请输入第二个数字：')
+
+# 求和
+sum_value = float(num1) + float(num2)
+
+print('数字 {0} 和数字 {1} 相加的结果为: {2} '.format(num1,num2,sum_value))
+print('两数之和为 %.1f' %(float(input('输入第一个数字：'))+float(input('输入第二个数字：'))))
+
+
+
+
+
